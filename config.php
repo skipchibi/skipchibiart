648b2102efe12c7b828b234a46b440186b05ad45
skipchibi <takase@yu-r.net>
2021-08-18 22:54:10 +0900

    add view/head.html

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
 
-define('DSN', '');
-define('DB_USER', '');
-define('DB_PASSWORD', '');
+define('TITLE','素材サイト');
+define('DESCRIPTION','無料素材を配布しています');
+define('KEYWORDS','素材,無料');
+define('DSN', 'mysql:dbname=skipchibiart;host=127.0.0.1;charset=utf8');
+define('DB_USER', 'skipchibiart');
+define('DB_PASSWORD', '7-evLwMb76xo6.6O');
 
 
 error_reporting(E_ALL & ~E_NOTICE);
